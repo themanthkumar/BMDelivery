2b6cf8b2021e43c6708a623d79508fed98d0729b
themanthkumar <themanth2012@gmail.com>
2017-06-24 03:40:36 +0530

    v 1
    bmd

---
+++ www/js/config.js
@@ -0,0 +1,7 @@
+/*Kartero App Configuration*/
+var krms_driver_config ={		
+	'ApiUrl':"https://budgetmela.com/delivery/api",					
+	'DialogDefaultTitle':"BMDelivery",	
+	'PushProjectID':"261945113164",	
+	'APIHasKey':""
+};
